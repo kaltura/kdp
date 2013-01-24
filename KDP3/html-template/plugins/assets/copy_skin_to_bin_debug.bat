5e64ef880a49494acfc3c270ca033ea36dff0189
Michal Radwantzor <michal.radwantzor@kaltura.com>
2013-01-24 17:40:08 +0200

    update gitignore

--- KDP3/html-template/plugins/assets/copy_skin_to_bin_debug.bat
+++
@@ -1,2 +0,0 @@
-del C:\ws\KDP_3\bin-debug\\assets\skin.swf
-copy C:\ws\KDP_3\src\assets\skin.swf C:\ws\KDP_3\bin-debug\\assets\skin.swf
--- KDP3/html-template/plugins/com/kaltura/kdpfl/plugin/component/copy_to_bin_debug.bat
+++
@@ -1,6 +0,0 @@
-cd C:\workspace\KDP_3\bin-debug\plugins
-del *.* /q
-del C:\workspace\KDP_3\bin-debug\config.xml /q
-cd C:\workspace\KDP_3\html-template\plugins
-copy *.* C:\workspace\KDP_3\bin-debug\plugins
-copy C:\workspace\KDP_3\html-template\config.xml C:\workspace\KDP_3\bin-debug\
